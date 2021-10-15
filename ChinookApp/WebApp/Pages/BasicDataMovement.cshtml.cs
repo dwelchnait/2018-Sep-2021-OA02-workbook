75e960d6a6bb53368174364f245c4eaae303d604
Don Welch <dwelch@nait.ca>
2021-10-15 11:40:40 -0600

    fianlize web app setup with holidaycss menu, investigatin OnGet

--- ChinookApp/WebApp/Pages/BasicDataMovement.cshtml.cs
+++ ChinookApp/WebApp/Pages/BasicDataMovement.cshtml.cs
@@ -9,8 +9,24 @@ namespace WebApp.Pages
 {
     public class BasicDataMovementModel : PageModel
     {
+        //data field
+        public string MyName;
+
         public void OnGet()
-        {
+        { 
+            //execute in response to a Get request from the user (browser)
+            //when the page is first accessed, it issues a Get Request
+            //when the page is refreshed, WITHOUT a Post, it issues a Get Request
+            Random rnd = new Random();
+            int oddeven = rnd.Next(0, 25);
+            if (oddeven % 2 == 0)
+            {
+                MyName = $"Don is {oddeven}";
+            }
+            else
+            {
+                MyName = null;
+            }
         }
     }
 }
