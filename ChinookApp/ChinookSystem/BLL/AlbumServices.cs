67aa56ebdbc7e3b951a203fc935ce7a800a9f112
Don Welch <dwelch@nait.ca>
2021-11-02 11:08:53 -0600

    Add methods (services) to AlbumServices for DRUD

--- ChinookApp/ChinookSystem/BLL/AlbumServices.cs
+++ ChinookApp/ChinookSystem/BLL/AlbumServices.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 #region Additional 
 using ChinookSystem.Models;
 using ChinookSystem.DAL;
+using ChinookSystem.Entities;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 #endregion
 
 namespace ChinookSystem.BLL
@@ -73,6 +75,125 @@ namespace ChinookSystem.BLL
             //we will inform sql to Skip(n rows) and Take(pagesize rows)
             return albums.Skip(skipRows).Take(pagesize).ToList();
         }
+
+        public List<AlbumItem> Albums_GetAlbumsByTitle(string title,
+                                     int pageNumber,
+                                     int pagesize,
+                                     out int totalcount)
+        {
+            IEnumerable<AlbumItem> albums = _context.Albums
+                                .Where(x => x.Title.Contains(title))
+                                .Select(x => new AlbumItem
+                                {
+                                    AlbumId = x.AlbumId,
+                                    Title = x.Title,
+                                    ArtistId = x.ArtistId,
+                                    ReleaseYear = x.ReleaseYear,
+                                    ReleaseLabel = x.ReleaseLabel
+                                })
+                                .OrderBy(x => x.Title);
+            totalcount = albums.Count();
+            int skipRows = (pageNumber - 1) * pagesize;
+            return albums.Skip(skipRows).Take(pagesize).ToList();
+        }
+        #endregion
+
+        #region Add,Update and Delete
+        public int AddAlbum(AlbumItem item)
+        {
+            //this method will return the new album id if the add was successful
+            //REMINDER: AlbumItem is NOT an entity; it is a view model class
+            //          This means you MUST move the data from the view model class
+            //              to an instance of your desired entity
+
+            //add a business rule to the method
+            //     rule: no album with the same title, same year, same artist
+            //     result: this will be considered a duplicate album
+
+            //how can one do such a test
+            //1) use a search loop pattern: set a flag as found or not found
+            //2) use can use Linq and test the result of a query: .FirstOrDefault()
+            Album exist = _context.Albums
+                            .Where(x => x.Title.Equals(item.Title)
+                                     && x.ArtistId == item.ArtistId
+                                     && x.ReleaseYear == item.ReleaseYear)
+                            .FirstOrDefault();
+            if (exist == null)
+            {
+                throw new Exception("Album already exists on file");
+            }
+
+            //setup the entity instance with the data from the view model parameter
+            //NOTE: Album has a identity pkey; therefore one does NOT need to set
+            //      the AlbumId
+            exist = new Album
+            {
+                Title = item.Title,
+                ArtistId = item.ArtistId,
+                ReleaseYear = item.ReleaseYear,
+                ReleaseLabel = item.ReleaseLabel
+            };
+            //stage add in local memory
+            _context.Add(exist);
+            //do any validation within the entity (validation anotation)
+            //send stage request to the database for processing
+            _context.SaveChanges();
+            return exist.AlbumId;
+        }
+        public int UpdateAlbum(AlbumItem item)
+        {
+            Album exist = _context.Albums
+                            .Where(x => x.AlbumId == item.AlbumId)
+                            .FirstOrDefault();
+            if (exist == null)
+            {
+                throw new Exception("Album does not exist on file");
+            }
+            //setup the entity instance with the data from the view model parameter
+            //NOTE: For an update you need the pkey value
+            exist = new Album
+            {
+                AlbumId = item.AlbumId,
+                Title = item.Title,
+                ArtistId = item.ArtistId,
+                ReleaseYear = item.ReleaseYear,
+                ReleaseLabel = item.ReleaseLabel
+            };
+            //stage add in local memory
+            EntityEntry<Album> updating = _context.Entry(exist);
+            updating.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            //do any validation within the entity (validation anotation)
+            //send stage request to the database for processing
+            //the returned value is the number of rows altered
+            return _context.SaveChanges();
+            
+        }
+
+        public int DeleteAlbum(AlbumItem item)
+        {
+            Album exist = _context.Albums
+                            .Where(x => x.AlbumId == item.AlbumId)
+                            .FirstOrDefault();
+            if (exist == null)
+            {
+                throw new Exception("Album already has been removed from the file");
+            }
+            exist = new Album
+            {
+                AlbumId = item.AlbumId,
+                Title = item.Title,
+                ArtistId = item.ArtistId,
+                ReleaseYear = item.ReleaseYear,
+                ReleaseLabel = item.ReleaseLabel
+            };
+            //stage add in local memory
+            EntityEntry<Album> deleting = _context.Entry(exist);
+            deleting.State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            //send stage request to the database for processing
+            //the returned value is the number of rows altered
+            return _context.SaveChanges();
+        }
+
         #endregion
     }
 }
