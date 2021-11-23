1c9e7a6bc4ef87222e0caa6c72bd8da8025ce96b
Don Welch <dwelch@nait.ca>
2021-11-23 11:45:16 -0700

    Re #3 Add Track method, logic to prepare for adding track

--- ChinookApp/ChinookSystem/BLL/PlaylistTrackServices.cs
+++ ChinookApp/ChinookSystem/BLL/PlaylistTrackServices.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 #region Additional 
 using ChinookSystem.Models;
 using ChinookSystem.DAL;
+using ChinookSystem.Entities;
 #endregion
 
 
@@ -47,7 +48,68 @@ namespace ChinookSystem.BLL
                                            string username,
                                            int trackid)
         {
-            throw new Exception("adding track BLL");
+            Playlist playlistExist = null;
+            PlaylistTrack playlisttrackExist = null;
+            int tracknumber = 0;
+
+            //Business Logic is the rules of your business
+            //   rule: a track can only exist once on a playlist
+            //   rule: each track on a playlist is assigned a continuous
+            //          track number
+            //
+            //The BLL method should also ensure that data exists for
+            //   the procesing of the transaction
+
+            //check my input to this method to ensure I have data
+            if (string.IsNullOrWhiteSpace(playlistname))
+            {
+                throw new Exception("Playlist name is missing. Unable to add track.");
+            }
+
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                throw new Exception("User name is missing. Unable to add track.");
+            }
+
+            //does the playlist exist?
+            playlistExist = _context.Playlists
+                                    .Where(x => x.Name.Equals(playlistname)
+                                             && x.UserName.Equals(username))
+                                    .FirstOrDefault();
+            if (playlistExist == null)
+            {
+                //no existing playlist, create one
+                playlistExist = new Playlist()
+                {
+                    Name = playlistname,
+                    UserName = username
+                };
+                _context.Playlists.Add(playlistExist);
+                tracknumber = 1;                
+            }
+            else
+            {
+                playlisttrackExist = _context.PlaylistTracks
+                           .Where(x => x.Playlist.Name.Equals(playlistname)
+                                    && x.Playlist.UserName.Equals(username)
+                                    && x.TrackId == trackid)
+                           .FirstOrDefault();
+                if (playlisttrackExist != null)
+                {
+                    throw new Exception("TracK already on playlist");
+                }
+                else
+                {
+                    //find the next tracknumber
+                    //do this automatically for the system
+                    tracknumber = _context.PlaylistTracks
+                           .Where(x => x.Playlist.Name.Equals(playlistname)
+                                    && x.Playlist.UserName.Equals(username))
+                           .Select(x => x.TrackNumber)
+                           .Max();
+                    tracknumber++;
+                }
+            }
         }
         #endregion
     }
