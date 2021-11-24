2fbaddda3133e29db6f48ddf425b07268169878f
Don Welch <dwelch@nait.ca>
2021-11-24 08:45:02 -0700

    Close #5 complete BLL AddTrack transaction

--- ChinookApp/ChinookSystem/BLL/PlaylistTrackServices.cs
+++ ChinookApp/ChinookSystem/BLL/PlaylistTrackServices.cs
@@ -59,6 +59,10 @@ namespace ChinookSystem.BLL
             //
             //The BLL method should also ensure that data exists for
             //   the procesing of the transaction
+            //
+            //If the business are passed and data is validation
+            //  a)stage your transaction work
+            //  b)execute a SINGLE .SaveChanges()
 
             //check my input to this method to ensure I have data
             if (string.IsNullOrWhiteSpace(playlistname))
@@ -84,6 +88,7 @@ namespace ChinookSystem.BLL
                     Name = playlistname,
                     UserName = username
                 };
+                //staged ONLY (in memory)
                 _context.Playlists.Add(playlistExist);
                 tracknumber = 1;                
             }
@@ -96,7 +101,7 @@ namespace ChinookSystem.BLL
                            .FirstOrDefault();
                 if (playlisttrackExist != null)
                 {
-                    throw new Exception("TracK already on playlist");
+                    throw new Exception("Track already on playlist");
                 }
                 else
                 {
@@ -110,6 +115,47 @@ namespace ChinookSystem.BLL
                     tracknumber++;
                 }
             }
+            //add the track to the playlist
+            //create an instance for the track
+            playlisttrackExist = new PlaylistTrack();
+
+            //load the properties (fields) of the instance
+            playlisttrackExist.TrackId = trackid;
+            playlisttrackExist.TrackNumber = tracknumber;
+
+            //??? what about the second part of the primary key: playlist id
+            //if the playlist exists then we know th id: playlistExist
+            //BUT if the playlist is NEW, we DO NOT know the id
+
+            //in the situation of a NEW playlist, even though we have
+            //  created the playlist instance (see above) it is ONLY
+            //  staged.
+            //this means that the actual sql record has NOT yet been 
+            //  created.
+            //this means that the IDENTITY value for the new playlist DOES
+            //  NOT yet exist. The value on the playlist instance (playlistExist)
+            //  is zero (0).
+            //Therefore we have a serious problem
+
+            //Solution:
+            //  Is built into the EntityFramework and is based on using the
+            //  navigational property in Playlist pointing to its "child"
+
+            //staging a typical Add in the pass was to reference the entity
+            //   and use the .Add(xxx)
+            //_context.PlaylistTracks.Add(playlistExists)
+            //IF you use this statement the playlistid would be zero(0) 
+            //    causing your transaction to ABORT
+            //Why? playlistExists.PlaylistId is currently zero.
+
+            //INSTEAD, do the staging using the parent.navproperty.Add(xxx)
+            playlistExist.PlaylistTracks.Add(playlisttrackExist);
+
+            //Staging is complete
+            //COMMIT the TRANSACTION
+            //to commit work to the sql database, use .SaveChanges()
+            //when doing transaction use ONE and ONLY ONE .SaveChanges()
+            _context.SaveChanges();
         }
         #endregion
     }
