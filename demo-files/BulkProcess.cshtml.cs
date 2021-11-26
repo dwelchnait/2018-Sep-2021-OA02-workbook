df1ae974b55ffb1d73e3b730ebc6da2865a30fb5
Don Welch <dwelch@nait.ca>
2021-11-26 05:33:15 -0700

    update demo files

--- demo-files/BulkProcess.cshtml.cs
+++ demo-files/BulkProcess.cshtml.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ChinookSystem.BLL;
-using ChinookSystem.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#region Additional Namespaces
+using ChinookSystem.BLL;
+using ChinookSystem.Models;
+using Microsoft.Extensions.Logging;
 using WebApp.Helpers;
+#endregion
+
 
 namespace WebApp.Pages
 {
@@ -15,114 +20,145 @@ namespace WebApp.Pages
         #region Private variables and DI constructor
         private readonly TrackServices _trackservices;
         private readonly PlaylistTrackServices _playlisttrackservices;
-
         [TempData]
         public string FeedBackMessage { get; set; }
-        public bool HasFeedBack => !string.IsNullOrWhiteSpace(FeedBackMessage);
-
         [TempData]
         public string ErrorMessage { get; set; }
 
+        //a get property that returns the result of the lamda action
         public bool HasError => !string.IsNullOrWhiteSpace(ErrorMessage);
+        public bool HasFeedBack => !string.IsNullOrWhiteSpace(FeedBackMessage);
 
-        public BulkProcessModel(TrackServices trackservices,
+        public BulkProcessModel(TrackServices trackservices, 
                                 PlaylistTrackServices playlisttrackservices)
         {
             _trackservices = trackservices;
             _playlisttrackservices = playlisttrackservices;
+
         }
         #endregion
 
-        //routed value from another page
-        [BindProperty(SupportsGet = true)]
+        [BindProperty(SupportsGet =true)]
         public string argsearch { get; set; }
 
-        //routed value from another page
         [BindProperty(SupportsGet = true)]
         public string argvalue { get; set; }
-
-        //fouted value local
-        [BindProperty(SupportsGet = true)]
-        public string playlistname { get; set; }
 
         [BindProperty]
         public int addtrackid { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string playlistname { get; set; }
+
+
         [BindProperty]
         public List<TrackInfo> trackInfo { get; set; }
 
+        //for two way binding MAKE SURE you have a bond property
         [BindProperty]
         public List<PLTrackInfo> pltrackInfo { get; set; }
 
-        //paging
-        private const int PAGE_SIZE = 5;
+        [BindProperty]
+        public List<CTrackInfo> ctrackInfo { get; set; }
+        #region Paginator
+        private const int PAGE_SIZE = 10;
         public Paginator Pager { get; set; }
-
+        #endregion
         public void OnGet(int? currentPage)
         {
-            if(argsearch != null && argvalue != null)
+            if (argsearch != null && argvalue !=null)
             {
+                //installing paginator
                 int pageNumber = currentPage.HasValue ? currentPage.Value : 1;
+                // call your service to get the data & the total count
                 PageState current = new(pageNumber, PAGE_SIZE);
                 int totalcount;
-                trackInfo = _trackservices.Tracks_GetByArtistAlbum(argsearch, argvalue,
-                                            pageNumber, PAGE_SIZE, out totalcount);
-                Pager = new Paginator(totalcount, current);
+                trackInfo = _trackservices.GetTracksArtistAlbum(argsearch,argvalue,
+                                                        pageNumber, PAGE_SIZE,
+                                                        out totalcount);
+                Pager = new(totalcount, current);
             }
-
-            if(playlistname != null)
+            if (playlistname != null)
             {
-                pltrackInfo = _playlisttrackservices.Tracks_GetPlaylistforUser(playlistname, "HansenB");
+                pltrackInfo = _playlisttrackservices.GetPLTracksforUser(playlistname, "HansenB");
             }
         }
-
-        public IActionResult OnPostFetch()
-        {
-            if (string.IsNullOrWhiteSpace(playlistname))
-            {
-                ErrorMessage = "Enter a playlist name to fetch.";
-            }
-            return RedirectToPage(new
-            {
-                argsearch = argsearch,
-                argvalue = argvalue,
-                playlistname = playlistname
-            });
-        }
-        
         public IActionResult OnPostAddTrack()
         {
             try
             {
                 if (string.IsNullOrWhiteSpace(playlistname))
                 {
-                    throw new Exception("You need to have a playlist select first");
+                    throw new Exception($"You need to have a playlist selected first");
                 }
-                //TODO: add track to playlist
-                _playlisttrackservices.PlaylistTrack_AddTrack(playlistname,
-                                                              "HansenB",
-                                                              addtrackid);
-                FeedBackMessage = "adding the track";
+                _playlisttrackservices.Add_TrackToPLaylist(playlistname,
+                    "HansenB", addtrackid);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 ErrorMessage = GetInnerException(ex).Message;
+                
             }
             return RedirectToPage(new
             {
                 argsearch = argsearch,
                 argvalue = argvalue,
+                playlistname =playlistname
+            }); 
+        }
+        public IActionResult OnPostFetch()
+        {
+            if (string.IsNullOrWhiteSpace(playlistname))
+            {
+                ErrorMessage = $"You need to have a playlist name";
+                
+            }
+            return RedirectToPage(new
+            {
+
+                argsearch = argsearch,
+                argvalue = argvalue,
                 playlistname = playlistname
             });
         }
+        public IActionResult OnPostRemove()
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(playlistname))
+                {
+                    ErrorMessage = $"you need to have a playlist name";
+                }
+                else
+                {
+                     _playlisttrackservices.DeleteTracks(playlistname, "HansenB", ctrackInfo);
+                     FeedBackMessage = "Tracks removed.";
+                    //FeedBackMessage = $"{ctrackInfo.Count()}";
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage = GetInnerException(ex).Message;
 
+            }
+
+            //return Page();
+            return RedirectToPage(new
+            {
+
+                argsearch = argsearch,
+                argvalue = argvalue,
+                playlistname = playlistname
+            });
+        }
         private Exception GetInnerException(Exception ex)
         {
-            while(ex.InnerException !=null)
+            while (ex.InnerException != null)
             {
                 ex = ex.InnerException;
             }
             return ex;
         }
+
     }
 }
--- demo-files/PlaylistTrackServices.cs
+++ demo-files/PlaylistTrackServices.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-#region Additional 
-using ChinookSystem.Models;
+#region Additional Namespaces
 using ChinookSystem.DAL;
 using ChinookSystem.Entities;
+using ChinookSystem.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 #endregion
 
 
@@ -15,9 +16,12 @@ namespace ChinookSystem.BLL
 {
     public class PlaylistTrackServices
     {
-        #region Constructor and DI variable setup
+        #region Constructor and Dependency Injection fields
         private readonly ChinookContext _context;
-
+        //todo:
+        // - get the Database context
+        // - Chinookcontext is internal
+        // - thus constructor needs to be internal for access match
         internal PlaylistTrackServices(ChinookContext context)
         {
             _context = context;
@@ -25,8 +29,9 @@ namespace ChinookSystem.BLL
         #endregion
 
         #region Queries
-        public List<PLTrackInfo> Tracks_GetPlaylistforUser(string playlistname, string username)
+        public List<PLTrackInfo> GetPLTracksforUser(string playlistname, string username)
         {
+
             IEnumerable<PLTrackInfo> info = _context.PlaylistTracks
                             .Where(x => x.Playlist.Name.Equals(playlistname) 
                                      && x.Playlist.UserName.Equals(username))
@@ -34,129 +39,215 @@ namespace ChinookSystem.BLL
                             {
                                 TrackId = x.TrackId,
                                 TrackNumber = x.TrackNumber,
-                                Song = x.Track.Name,
+                                Name = x.Track.Name,
                                 Milliseconds = x.Track.Milliseconds
-
+                                
                             })
                             .OrderBy(x => x.TrackNumber);
+            
             return info.ToList();
         }
         #endregion
 
-        #region Playlist Track Maintenance
-        public void PlaylistTrack_AddTrack(string playlistname,
-                                           string username,
-                                           int trackid)
+        #region Maintain Playlist
+        public void Add_TrackToPLaylist(string playlistname, string username, int trackid)
         {
-            Playlist playlistExist = null;
-            PlaylistTrack playlisttrackExist = null;
+            Playlist playlistExists = null;
+            PlaylistTrack playlisttrackExists = null;
             int tracknumber = 0;
-
-            //Business Logic is the rules of your business
-            //   rule: a track can only exist once on a playlist
-            //   rule: each track on a playlist is assigned a continuous
-            //          track number
-            //
-            //The BLL method should also ensure that data exists for
-            //   the procesing of the transaction
-            //
-            //If the business are passed and data is validation
-            //  a)stage your transaction work
-            //  b)execute a SINGLE .SaveChanges()
-
-            //check my input to this method to ensure I have data
-            if (string.IsNullOrWhiteSpace(playlistname))
-            {
-                throw new Exception("Playlist name is missing. Unable to add track.");
-            }
-
-            if (string.IsNullOrWhiteSpace(username))
-            {
-                throw new Exception("User name is missing. Unable to add track.");
-            }
-
-            //does the playlist exist?
-            playlistExist = _context.Playlists
-                                    .Where(x => x.Name.Equals(playlistname)
-                                             && x.UserName.Equals(username))
-                                    .FirstOrDefault();
-            if (playlistExist == null)
-            {
-                //no existing playlist, create one
-                playlistExist = new Playlist()
+           
+                //This class is in what is called the Business Logic Layer
+                //Business Logic is the rules of your business
+                //  rule: a track can only exist once on a playlist
+                //  rule: each track on a playlist is assigned a continious
+                //        track number
+                //
+                //The BLL method should also ensure that data exists for
+                //   the processing of the transaction
+                if (string.IsNullOrEmpty(playlistname))
                 {
-                    Name = playlistname,
-                    UserName = username
-                };
-                //staged ONLY (in memory)
-                _context.Playlists.Add(playlistExist);
-                tracknumber = 1;                
-            }
-            else
-            {
-                playlisttrackExist = _context.PlaylistTracks
-                           .Where(x => x.Playlist.Name.Equals(playlistname)
-                                    && x.Playlist.UserName.Equals(username)
-                                    && x.TrackId == trackid)
-                           .FirstOrDefault();
-                if (playlisttrackExist != null)
+                    //there is a data error
+                    
+                    throw new Exception("Playlist name is missing. Unable to add track");
+                }
+                if (string.IsNullOrEmpty(username))
                 {
-                    throw new Exception("Track already on playlist");
+                    //there is a data error
+                    throw new Exception("User name was not supplied");
+                }
+
+                //does the playlist exist?
+                playlistExists = (from x in _context.Playlists
+                                  where (x.Name.Equals(playlistname)
+                                      && x.UserName.Equals(username))
+                                  select x).FirstOrDefault();
+                if (playlistExists == null)
+                {
+                    //the playlist DOES NOT exists
+                    //tasks:
+                    //      create a new instance of a playlist object
+                    //      load the instance with data
+                    //      stage the add of the new instance
+                    //      set a variable representing the tracknumber to 1
+                    playlistExists = new Playlist()
+                    {
+                        Name = playlistname,
+                        UserName = username
+                    };
+                    _context.Playlists.Add(playlistExists); //stage ONLY!!!!!!!!!!
+                    tracknumber = 1;
                 }
                 else
                 {
-                    //find the next tracknumber
-                    //do this automatically for the system
-                    tracknumber = _context.PlaylistTracks
-                           .Where(x => x.Playlist.Name.Equals(playlistname)
-                                    && x.Playlist.UserName.Equals(username))
-                           .Select(x => x.TrackNumber)
-                           .Max();
+                    //the playlist already exists
+                    //verify track not already on playlist (business rule)
+                    //what is the next tracknumber
+                    //add 1 to the tracknumber
+                    playlisttrackExists = (from x in _context.PlaylistTracks
+                                           where x.Playlist.Name.Equals(playlistname)
+                                               && x.Playlist.UserName.Equals(username)
+                                               && x.TrackId == trackid
+                                           select x).FirstOrDefault();
+                    if (playlisttrackExists == null)
+                    {
+                        tracknumber = (from x in _context.PlaylistTracks
+                                       where x.Playlist.Name.Equals(playlistname)
+                                           && x.Playlist.UserName.Equals(username)
+                                       select x).Count();
+                        tracknumber++;
+                    }
+                    else
+                    {
+                        throw new Exception("Track already on playlist.");
+                    }
+                }
+
+                //create the playlist track
+                playlisttrackExists = new PlaylistTrack();
+
+                //load of the playlist track
+                playlisttrackExists.TrackId = trackid;
+                playlisttrackExists.TrackNumber = tracknumber;
+
+                //??????
+                //what is the playlist id
+                //if the playlist exists then we know the id
+                //BUT if the playlist is new, we DO NOT know the id
+
+                //in one case the id is known BUT in the second case
+                //    where the new record is ONLY STAGED, NO primary key
+                //    value has been generated yet.
+                //if you access the new playlist record the pkey would be 0 (default numeric)
+
+                //the solution to BOTH of these scenarios is to use
+                //    navigational properties during the ACTUAL .Add command
+                //    for the new playlisttrack record
+                //the entityframework will, on your behave, ensure that the adding
+                //      of records to the database will be done in the appropriate
+                //      order AND will add the missing compound primary key value
+                //      (PlaylistId) to the new playlisttrack record
+
+                //NOT LIKE this!!!! THIS IS WRONG!!!!!
+                //context.PlaylistTracks.Add(playlisttrackExists);
+
+                //INSTEAD, do the staging using the parent.navproperty.Add(xxxx)
+                playlistExists.PlaylistTracks.Add(playlisttrackExists);
+
+                
+                    //COMMIT THE TRANSACTION
+                    //the ALL the staged records to sql for processing
+                    _context.SaveChanges();
+                
+        }//eom
+
+
+        public void DeleteTracks(string playlistname, string username, List<CTrackInfo> trackstodelete)
+
+        {
+            //Vaidation data presents
+            if (string.IsNullOrEmpty(playlistname))
+            {
+                throw new Exception("Playlist name is missing. Unable to add track");
+            }
+            if (string.IsNullOrEmpty(username))
+            {
+                throw new Exception("User name was not supplied");
+            }
+            //count number of tracks selected for removal
+            //rule: must have at least one selected
+
+            //this is the test using a method declared in the record
+            if (trackstodelete.Where(x => x.Checked()).Select(x => x.TrackId).Count() == 0)
+            {
+                throw new Exception("You did not select any tracks to delete");
+            }
+
+            //this is the test using the datafield in the record
+            if (trackstodelete.Where(x => x.InputData != null).Select(x => x.TrackId).Count() == 0)
+            {
+                throw new Exception("You did not select any tracks to delete");
+            }
+
+
+            //get Playlist instance
+            Playlist exists = (from x in _context.Playlists
+                                where x.Name.Equals(playlistname)
+                                    && x.UserName.Equals(username)
+                                select x).FirstOrDefault();
+            //does playlist exist
+            //rule: if no playlist exists then there can be no playlist tracks
+            if (exists == null)
+            {
+                throw new Exception("Playlist does not exist");
+            }
+
+            //remove the desired tracks
+            //     11, 235, 34, ...
+            PlaylistTrack item = null;
+            foreach (var deletetrack in trackstodelete)
+            {
+                item = _context.PlaylistTracks
+                                .Where(tr => tr.Playlist.Name.Equals(playlistname)
+                                        && tr.Playlist.UserName.Equals(username)
+                                        && tr.TrackId == deletetrack.TrackId
+                                        && deletetrack.InputData != null)
+                                .Select(tr => tr).FirstOrDefault();
+                if (item != null)
+                {
+                    //staged
+                    //    parent.navproperty.Remove()
+                    exists.PlaylistTracks.Remove(item);
+                }
+            }
+
+            //re - sequence the kept tracks
+            
+            int tracknumber = 1;
+            foreach (var track in trackstodelete)
+            {
+
+                //list of all track that are to be kept
+                var trackskept = _context.PlaylistTracks
+                                    .Where(tr => tr.Playlist.Name.Equals(playlistname)
+                                            && tr.Playlist.UserName.Equals(username)
+                                             && tr.TrackId == track.TrackId
+                                            && track.InputData == null)
+                                    .Select(tr => tr).FirstOrDefault();
+                if (trackskept != null)
+                {
+                    trackskept.TrackNumber = tracknumber;
+                    _context.Entry(trackskept).Property(nameof(PlaylistTrack.TrackNumber)).IsModified = true;  //Staged
                     tracknumber++;
                 }
             }
-            //add the track to the playlist
-            //create an instance for the track
-            playlisttrackExist = new PlaylistTrack();
 
-            //load the properties (fields) of the instance
-            playlisttrackExist.TrackId = trackid;
-            playlisttrackExist.TrackNumber = tracknumber;
+            //save the work
 
-            //??? what about the second part of the primary key: playlist id
-            //if the playlist exists then we know th id: playlistExist
-            //BUT if the playlist is NEW, we DO NOT know the id
-
-            //in the situation of a NEW playlist, even though we have
-            //  created the playlist instance (see above) it is ONLY
-            //  staged.
-            //this means that the actual sql record has NOT yet been 
-            //  created.
-            //this means that the IDENTITY value for the new playlist DOES
-            //  NOT yet exist. The value on the playlist instance (playlistExist)
-            //  is zero (0).
-            //Therefore we have a serious problem
-
-            //Solution:
-            //  Is built into the EntityFramework and is based on using the
-            //  navigational property in Playlist pointing to its "child"
-
-            //staging a typical Add in the pass was to reference the entity
-            //   and use the .Add(xxx)
-            //_context.PlaylistTracks.Add(playlistExists)
-            //IF you use this statement the playlistid would be zero(0) 
-            //    causing your transaction to ABORT
-            //Why? playlistExists.PlaylistId is currently zero.
-
-            //INSTEAD, do the staging using the parent.navproperty.Add(xxx)
-            playlistExist.PlaylistTracks.Add(playlisttrackExist);
-
-            //Staging is complete
-            //COMMIT the TRANSACTION
-            //to commit work to the sql database, use .SaveChanges()
-            //when doing transaction use ONE and ONLY ONE .SaveChanges()
             _context.SaveChanges();
-        }
+            
+        }//eom
+
         #endregion
-    }
+    }//eoc
 }
+
